332301d38859bc7123fdd131db51b7b29e18c2de
Prashrijan <shresthaprashrijan@gmail.com>
2025-07-19 21:02:51 +1000

    resume card complete

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "~/../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -19,6 +21,13 @@ export default function Home() {
                         Review your submissions and check AI-powered feedback.
                     </h2>
                 </div>
+                {resumes.length > 0 && (
+                    <div className="resumes-section">
+                        {resumes.map((resume) => (
+                            <ResumeCard key={resume.id} resume={resume} />
+                        ))}
+                    </div>
+                )}
             </section>
         </main>
     );
