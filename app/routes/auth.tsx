dc0e47bc4c83e361382316e7bb7aa6e960e1f4b6
Prashrijan <shresthaprashrijan@gmail.com>
2025-07-20 23:14:05 +1000

    authentication setup with puterr

---
+++ app/routes/auth.tsx
@@ -0,0 +1,39 @@
+import { usePuterStore } from "lib/puter";
+import React from "react";
+
+export const meta = () => [
+    {
+        title: "Resumind | Auth",
+    },
+    {
+        name: "description",
+        content: "Log into your account",
+    },
+];
+
+const Auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue Your Job Journey</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                Signing you in....
+                            </button>
+                        ) : (
+                            <button className="auth-button">Sign In </button>
+                        )}
+                    </div>
+                </section>
+            </div>
+        </main>
+    );
+};
+
+export default Auth;
