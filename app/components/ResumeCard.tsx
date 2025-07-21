812a24f34541a3f823ea8307b96c6e3ec80572d8
Prashrijan <shresthaprashrijan@gmail.com>
2025-07-22 01:10:41 +1000

    fetch real images on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -28,12 +28,20 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
         >
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    <h2 className="!text-black font-bold break-words">
-                        {companyName}
-                    </h2>
-                    <h3 className="text-lg break-words text-gray-500">
-                        {jobTitle}
-                    </h3>
+                    {companyName && (
+                        <h2 className="!text-black font-bold break-words">
+                            {companyName}
+                        </h2>
+                    )}
+                    {jobTitle && (
+                        <h3 className="text-lg break-words text-gray-500">
+                            {jobTitle}
+                        </h3>
+                    )}
+
+                    {!companyName && !jobTitle && (
+                        <h2 className="!text-black font-bold">Resume</h2>
+                    )}
                 </div>
 
                 <div className="flex-shrink-0">
@@ -41,15 +49,17 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
                 </div>
             </div>
 
-            <div className="gradient-border animate-in fade-in duration-1000">
-                <div className="w-full h-full">
-                    <img
-                        src={imagePath}
-                        alt="resume"
-                        className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-                    />
+            {resumeUrl && (
+                <div className="gradient-border animate-in fade-in duration-1000">
+                    <div className="w-full h-full">
+                        <img
+                            src={resumeUrl}
+                            alt="resume"
+                            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                        />
+                    </div>
                 </div>
-            </div>
+            )}
         </Link>
     );
 };
